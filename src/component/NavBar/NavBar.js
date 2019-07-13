311207002d8db265b2128750118265d843eb91df
Uncle Sam <djiasara@gmail.com>
2019-07-12 20:24:52 -0400

    Nabar updating

--- src/component/NavBar/NavBar.js
+++ src/component/NavBar/NavBar.js
@@ -1,32 +1,38 @@
 import React from 'react';
-import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap';
 
 
 function NavBar(props) {
     return (
         <React.Fragment>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Container>
+                <Navbar.Brand href="#home">PO0 HAUT-NKAM</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <Nav className="ml-auto">
+                        <NavDropdown title="About US" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Our vision</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Executive Team</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">By Laws</NavDropdown.Item>
                         </NavDropdown>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#projects">Media</Nav.Link>
+                        <NavDropdown title="Media" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Photo Gallery</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Video Gallery</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#projects">Events</Nav.Link>
+                        <Nav.Link href="#projects">Membership</Nav.Link>
+                        <Nav.Link href="#projects">Report</Nav.Link>
+                        <Nav.Link href="#projects">Contact US</Nav.Link>
                         <Nav.Link eventKey={2} href="#memes">
                             Dank memes
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
+                </Container>
             </Navbar>
 
         </React.Fragment>
