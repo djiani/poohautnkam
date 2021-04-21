e4a0e63958b9211c46dda0724353f11e157f0f03
Samuel Raoul Djiani <djiasara@gmail.com>
2021-04-21 19:02:32 -0400

    resizing carousel

--- src/component/MainComponent/Media/Gallery/Gallery.js
+++ src/component/MainComponent/Media/Gallery/Gallery.js
@@ -21,31 +21,34 @@ export default function Gallery() {
                         <ResponsiveEmbed aspectRatio="4by3" >
                             <embed  src={img[index].url} />
                         </ResponsiveEmbed>
-                        <Alert.Link href="https://sergemaillimediastudio.zenfolio.com/p370874366/ecc43121c">an example link</Alert.Link>.
-     
+                        <Alert.Link href="https://sergemaillimediastudio.zenfolio.com/p370874366/ecc43121c">View More...</Alert.Link>
                 </Alert>
             );
           }
           return( 
-              <Row className="mt-2 text-center" >
-                <Col sm={6} md={4} >
-                    <button  onClick={() => {setShow(true); setIndex(0)}} >
-                        <img src="https://sergemaillimediastudio.zenfolio.com/img/s/v-10/p3426947615-2.jpg" width="100%"  alt={img[0].msg} className="img-thumbnail" />
-                    </button>
-                    <a href={img[0].url} target="_blank" rel="noreferrer"><p>{img[0].msg}</p></a>
-                </Col>
-                <Col sm={6} md={4}>
-                    <button  onClick={() => {setShow(true); setIndex(1)}} >
-                        <img src="https://sergemaillimediastudio.zenfolio.com/img/s/v-10/p2150553798-2.jpg" width="100%"  alt={img[1].msg}  className="img-thumbnail"/>
-                    </button>
-                    <a href={img[1].url} target="_blank" rel="noreferrer"><p>{img[1].msg}</p></a>
-                </Col>
-                <Col sm={6} md={4}>
-                    <button  onClick={() => {setShow(true); setIndex(2)}} >
-                        <img src="https://sergemaillimediastudio.zenfolio.com/img/s/v-10/p2699230092-2.jpg" width="100%"  alt={img[2].msg} className="img-thumbnail" />
-                    </button>
-                    <a href={img[2].url} target="_blank" rel="noreferrer"><p>{img[2].msg}</p></a>
-                </Col>
-            </Row>);
+            <div className="text-center">
+                <h3>Poo Hautnkam Gallery photos by Serge Media Studio </h3>
+                <Row className="mt-2 text-center">
+                    <Col sm={6} md={4} >
+                        <button  onClick={() => {setShow(true); setIndex(0)}} >
+                            <img src="https://sergemaillimediastudio.zenfolio.com/img/s/v-10/p3426947615-2.jpg" width="100%"  alt={img[0].msg} className="img-thumbnail" />
+                        </button>
+                        <a href={img[0].url} target="_blank" rel="noreferrer"><p>{img[0].msg}</p></a>
+                    </Col>
+                    <Col sm={6} md={4}>
+                        <button  onClick={() => {setShow(true); setIndex(1)}} >
+                            <img src="https://sergemaillimediastudio.zenfolio.com/img/s/v-10/p2150553798-2.jpg" width="100%"  alt={img[1].msg}  className="img-thumbnail"/>
+                        </button>
+                        <a href={img[1].url} target="_blank" rel="noreferrer"><p>{img[1].msg}</p></a>
+                    </Col>
+                    <Col sm={6} md={4}>
+                        <button  onClick={() => {setShow(true); setIndex(2)}} >
+                            <img src="https://sergemaillimediastudio.zenfolio.com/img/s/v-10/p2699230092-2.jpg" width="100%"  alt={img[2].msg} className="img-thumbnail" />
+                        </button>
+                        <a href={img[2].url} target="_blank" rel="noreferrer"><p>{img[2].msg}</p></a>
+                    </Col>
+                </Row>
+            </div>
+            );
     }
     
