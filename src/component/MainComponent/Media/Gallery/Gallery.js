2bcab20cdbeac75e5d704046bb8044c7d968eda6
Samuel Raoul Djiani <djiasara@gmail.com>
2021-04-21 16:44:53 -0400

    adding target reference to link

--- src/component/MainComponent/Media/Gallery/Gallery.js
+++ src/component/MainComponent/Media/Gallery/Gallery.js
@@ -1,5 +1,5 @@
 import React, {useState } from "react";
-import {Row, Col, Alert, Button, ResponsiveEmbed} from 'react-bootstrap';
+import {Row, Col, Alert, ResponsiveEmbed} from 'react-bootstrap';
 
 export default function Gallery() {
     
@@ -32,19 +32,19 @@ export default function Gallery() {
                     <button  onClick={() => {setShow(true); setIndex(0)}} >
                         <img src="https://sergemaillimediastudio.zenfolio.com/img/s/v-10/p3426947615-2.jpg" width="100%"  alt={img[0].msg} className="img-thumbnail" />
                     </button>
-                    <p>{img[0].msg}</p>
+                    <a href={img[0].url} target="_blank" rel="noreferrer"><p>{img[0].msg}</p></a>
                 </Col>
                 <Col sm={6} md={4}>
                     <button  onClick={() => {setShow(true); setIndex(1)}} >
                         <img src="https://sergemaillimediastudio.zenfolio.com/img/s/v-10/p2150553798-2.jpg" width="100%"  alt={img[1].msg}  className="img-thumbnail"/>
                     </button>
-                    <p>{img[1].msg}</p>
+                    <a href={img[1].url} target="_blank" rel="noreferrer"><p>{img[1].msg}</p></a>
                 </Col>
                 <Col sm={6} md={4}>
                     <button  onClick={() => {setShow(true); setIndex(2)}} >
                         <img src="https://sergemaillimediastudio.zenfolio.com/img/s/v-10/p2699230092-2.jpg" width="100%"  alt={img[2].msg} className="img-thumbnail" />
                     </button>
-                    <p>{img[1].msg}</p>
+                    <a href={img[2].url} target="_blank" rel="noreferrer"><p>{img[2].msg}</p></a>
                 </Col>
             </Row>);
     }
